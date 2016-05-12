3821b9ff5c8dd7f99c2495736ee221ead1ac0d62
Kasper Juel <kaped82@gmail.com>
2016-05-12 16:28:56 +0200

    whoops, fixed issue with scroll animation

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,13 @@ $(window).scroll(function() {
 	var scrollDown = $(this).scrollTop();
 	var titleAndSubTitle = $('.title,.sub_title');
 
-	$('header > .content').css({
-		'transform' : 'translate(0,' + scrollDown /34 + '%)'
+
+	$(titleAndSubTitle).css({
+		'transform' : 'translate(0,' + scrollDown / 34 + '%)'
 	});
 
-	$(titleAndSubTitle).css('opacity', 1 - scrollDown / 320);
-	
+	$(titleAndSubTitle).css(
+		'opacity', 1 - scrollDown / 320
+	);
+
 });
